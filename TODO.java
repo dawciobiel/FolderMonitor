a5a412bb9caafc08bf46965ed9a00544c10278ef
Dawid Bielecki dawciobielEU <bischop.eu@gmail.com>
2022-06-05 14:51:27 +0200

    refactoring Add TODO.java as task list file

---
+++ TODO.java
@@ -0,0 +1,13 @@
+public class TODO {
+    // todo Create feature for output (count.txt file)
+    /* todo Check and refactor if needed all language bundles
+            1) wymyślić system oznaczania label'ek
+             ERROR _ xx _
+             INFO_ ...
+    */
+    /* todo Logger is not working
+            Maybe .xml config is needed or proper .properties config
+            Do it by tutorial: https://mkyong.com/logging/apache-log4j-2-tutorials/
+     */
+    // // TODO:
+}
