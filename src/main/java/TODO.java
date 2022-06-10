f3c612bf487a6e27dffbbafce54069fc367b61c8
Dawid Bielecki dawciobiel <dawciobiel@gmail.com>
2022-06-10 15:09:11 +0200

    update of the TODO

---
+++ src/main/java/TODO.java
@@ -0,0 +1,25 @@
+public class TODO {
+
+    /* fixme When a new file appears, FileMonitor will react repeatedly:
+    - a new file has been detected
+    - the file has been modified
+    - the file has been modified
+
+     I am not sure if this behavior is correct or what it causes.
+
+    If an existing file has been modified, FileMonitor will react twice:
+    - the file has been modified
+    - the file has been modified
+    */
+
+    /* todo Features list:
+    - Gson adding moved files count to the file (count.txt)
+    - Threads
+    - Tests JUnit
+    - Docs
+    - adding package-info.java files
+    - fix all problems from the code
+     */
+
+
+}
